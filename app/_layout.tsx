4827503699f696366c7abdc9c2979b45b1e1d57b
Hector Churquina <hectoruntdf@gmail.com>
2026-06-17 19:43:45 -0300

    chore(config): configurar TanStack Query e instalar dependencias

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 
 
 export default function RootLayoutNav() {
-  
+  const queryClient = new QueryClient();
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+      </Stack>
+    </QueryClientProvider>
   );
 }
